59e7dae4cae2d29353da83d12690257a71561b12
charles ono <investorshi20@gmail.com>
2026-02-28 05:15:24 -1200

    updated profile code

--- resources/views/dashboard/profile.blade.php
+++ resources/views/dashboard/profile.blade.php
@@ -136,7 +136,7 @@
                                     </div>
                                     <div>
                                         <label class="block font-semibold text-sm text-[#0C3A30] mb-2">Country</label>
-                                        <input type="text" name="country" value="{{ old('country', $user->country) }}" class="form-control custom-input" readonly class="form-control custom-input bg-gray-100 cursor-not-allowed"/>
+                                        <input type="text" name="country" value="{{ old('country', $user->country) }}" class="form-control custom-input"  class="form-control custom-input bg-gray-100 cursor-not-allowed"/>
                                     </div>
                                     <div>
                                         <label class="block font-semibold text-sm text-[#0C3A30] mb-2">Address</label>
