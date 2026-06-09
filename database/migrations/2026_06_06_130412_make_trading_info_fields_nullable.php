82feeeffe2786d0fe49ed39edb8fd9bee5316441
charles ono <investorshi20@gmail.com>
2026-06-08 14:27:52 -1200

    made trading info nullable

--- database/migrations/2026_06_06_130412_make_trading_info_fields_nullable.php
+++ database/migrations/2026_06_06_130412_make_trading_info_fields_nullable.php
@@ -9,16 +9,23 @@ return new class extends Migration
     /**
      * Run the migrations.
      */
-    public function up(): void
-    {
-        //
-    }
+ public function up()
+{
+    Schema::table('user_trading_infos', function (Blueprint $table) {
+        $table->string('annual_income')->nullable()->change();
+        $table->string('financial_alternative')->nullable()->change();
+        $table->string('ongoing_deposit_source')->nullable()->change();
+    });
+}
+
 
     /**
      * Reverse the migrations.
      */
     public function down(): void
     {
-        //
+        Schema::table('user_trading_infos', function (Blueprint $table) {
+            //
+        });
     }
 };
